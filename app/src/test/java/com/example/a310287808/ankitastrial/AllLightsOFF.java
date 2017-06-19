9ff834ae03e19a27c5a234aa32bb0979a00db421
AutomationTesting144 <automationtesting144@gmail.com>
2017-06-19 13:23:33 -0700

    Updated the comments

--- app/src/test/java/com/example/a310287808/ankitastrial/AllLightsOFF.java
+++ app/src/test/java/com/example/a310287808/ankitastrial/AllLightsOFF.java
@@ -4,6 +4,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,6 +14,7 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
 import io.appium.java_client.android.AndroidDriver;
 
 
@@ -34,19 +36,18 @@ public class AllLightsOFF {
     public int lightCounter=0;
     public String x;
 
-
+// Widget for switching the lights OFF is already created in the device as a precondition. This code will click on the widget and check
+// whether all the lights which are connected to the bridge are switched off or not.
     public void AllLightsONorOFF(AndroidDriver driver) throws IOException, JSONException, InterruptedException {
         driver.navigate().back();
-        System.out.println("Entered");
 
-        driver.findElement(By.id("com.ifttt.ifttt:id/widget_do_small_recipe_btn")).click();
+// Clicking on the widget created in the device
+        //driver.findElement(By.id("com.ifttt.ifttt:id/widget_do_small_recipe_btn")).click();
         WebElement abc = driver.findElement(By.xpath("//android.widget.ImageView[@bounds='[1002,1158][1158,1276]']"));
         abc.click();
-        System.out.println("Clicked");
-
-
         TimeUnit.SECONDS.sleep(20);
 
+        //Connecting with the API to fetch the status of the lights
         HttpURLConnection connection;
 
         finalURL = "http://" + IPAddress + "/" + HueUserName + "/" + HueBridgeParameterType;
@@ -72,12 +73,14 @@ public class AllLightsOFF {
 
         HashMap<String,Integer> lightIDs = new HashMap<>();
         lightIDs.put("1",1);
-        lightIDs.put("2",2);
-        lightIDs.put("4",3);
-        lightIDs.put("5",4);
-        lightIDs.put("29",5);
-        lightIDs.put("60",6);
-        lightIDs.put("59",7);
+        lightIDs.put("4",2);
+        lightIDs.put("26",3);
+        lightIDs.put("27",4);
+        lightIDs.put("28",5);
+        lightIDs.put("45",6);
+        lightIDs.put("30",7);
+        lightIDs.put("29",8);
+        lightIDs.put("31",9);
 
         //HashMap<String,Integer> FailedLights = new HashMap<>();
         StringBuffer sb = new StringBuffer();
@@ -109,11 +112,13 @@ public class AllLightsOFF {
                     StringBuffer br1 = new StringBuffer();
 
                     String line1 = " ";
+                String change = null;
                     while ((line1 = reader1.readLine()) != null) {
+                        //change = line1.replace("[", "").replace("]", "");
                         br1.append(line1);
                     }
                     String output1 = br1.toString();
-
+                System.out.println(output1);
                     JSONObject jsonObject = new JSONObject(output1);
 
 
@@ -121,7 +126,7 @@ public class AllLightsOFF {
                     String newString = ob.toString();
                     Object lightNameObject = jsonObject.get("name");
                     String lightName = lightNameObject.toString();
-
+                    System.out.println(lightName);
 
                     JSONObject jsonObject1 = new JSONObject(newString);
                     Object ob1 = jsonObject1.get("on");
